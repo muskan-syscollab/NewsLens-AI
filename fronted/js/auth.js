c4b2b3ea5e46d8dca8b5d7f3525ff4be710b82cf
Suman Sourabh <sumsourabh14@gmail.com>
2026-06-30 19:52:23 +0530

    Fixed auth API URLs

--- fronted/js/auth.js
+++ fronted/js/auth.js
@@ -7,7 +7,7 @@ if (loginForm) {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const response = await fetch("https://newslens-ais.onrender.com/login", {
+    const response = await fetch("/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
